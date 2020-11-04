aef5b80fc7316a72dd6b5a58a8ee135e1ba9f420
Holden Nelson <holden.nathaniel.nelson@gmail.com>
2020-11-04 11:37:25 -0700

    adds free videos to video page urls

--- find_urls.py
+++ find_urls.py
@@ -17,9 +17,29 @@ def find_urls():
     session = requests.session()
 
     # free videos
+    # start from page 2 and countdown
+    for i in range(2, 0, -1):
+        # request the page
+        video_episode_archive_page = session.get('https://www.harmontown.com/category/freevideos/page/'
+                                                 + str(i) + '/')
+
+        # build a tree out of its elements
+        video_episode_archive_page_tree = html.fromstring(video_episode_archive_page.content)
+
+        # build a list of the elements that contain links to
+        # individual pages
+        video_episode_page_links = video_episode_archive_page_tree.xpath('//h2[@class="entry-title"]/a')
+
+        # process each element
+        for link in video_episode_page_links:
+            # extract episode number from the title
+            episode_number = re.findall(pattern, link.text)[0]
+
+            # insert page link by episode number into dict
+            vid_page_urls_by_episode[episode_number] = link.get('href')
+            print(episode_number, vid_page_urls_by_episode[episode_number])
 
     # paid videos
-
     # start from page 23 and countdown
     for i in range(23, 0, -1):
         # request the page
